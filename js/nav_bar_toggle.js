2751022adec14941756410d41683b56a894b2423
JackRyan1989 <jack.jackryan@gmail.com>
2021-08-04 08:56:53 -0400

    Got rid of scroll effect on nav. More styling

--- js/nav_bar_toggle.js
+++ js/nav_bar_toggle.js
@@ -6,26 +6,39 @@ let nav = document.getElementById("navbar");
 let btn = document.getElementsByClassName("menu_button")[0];
 // The nav bar unordered list that is currently hidden
 let navUL = document.getElementById("nav__ul");
+// Nav menu items:
+let navItems = document.getElementsByClassName("nav_menu_item");
+
+function openNav() {
+  navUL.classList.remove("compressed");
+  navUL.classList.add("expanded");
+  nav.classList.remove("nav__compressed");
+  nav.classList.add("nav__expanded");
+  btn.setAttribute("data-open", "true");
+  btn.textContent = "Close";
+};
+
+function closeNav() {
+  navUL.classList.remove("expanded");
+  navUL.classList.add("compressed");
+  nav.classList.remove("nav__expanded");
+  nav.classList.add("nav__compressed");
+  btn.setAttribute("data-open", "false");
+  btn.textContent = "Open";
+};
 
 function expando() {
   let open = btn.getAttribute("data-open");
   if (open === "true") {
-    navUL.classList.remove("expanded");
-    navUL.classList.add("compressed");
-    nav.classList.remove("nav__expanded");
-    nav.classList.add("nav__compressed");
-    btn.setAttribute("data-open", "false");
-    btn.textContent = "Open";
+    closeNav();
   } else if (open === "false") {
-    navUL.classList.remove("compressed");
-    navUL.classList.add("expanded");
-    nav.classList.remove("nav__compressed");
-    nav.classList.add("nav__expanded");
-    btn.setAttribute("data-open", "true");
-    btn.textContent = "Close";
+    openNav();
   }
 }
 
 // Our onclick event:
 btn.onclick = expando;
 btn.onkeydown = expando;
+[].forEach.call(navItems, function(item) {
+  item.onclick = closeNav;
+});
