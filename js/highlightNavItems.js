66f7faa80bacbe82fff2373f09143a652e5b9000
JackRyan1989 <jackryan@navapbc.com>
2021-12-19 14:15:27 -0500

    Added intersection observer for cute lil highlight nav items

--- js/highlightNavItems.js
+++ js/highlightNavItems.js
@@ -1,13 +1,44 @@
-function createObserver() {
+var targets = [];
+var anchors = [];
+var sectionIds = document.querySelectorAll("section h2");
+var navAnchors = document.querySelectorAll(".nav_menu_item");
+
+sectionIds.forEach(function(section){
+  targets.push(section);
+});
+navAnchors.forEach(function(anchor){
+  anchors.push(anchor);
+});
+
+
+function handleIntersect(entries, observer) {
+    entries.forEach(function(entry){
+      if (entry.isIntersecting) {
+        anchors.forEach(function(anchor){
+          if (entry.target.innerHTML === anchor.innerHTML) {
+            anchor.classList.add("active");
+          } else {
+            anchor.classList.remove("active");
+          }
+        })
+      }
+    })
+};
+
+function createObserver(input) {
     let observer;
     let options = {
       root: null,
       rootMargin: "0px",
-      threshold: buildThresholdList()
+      threshold: 1
     };
   
     observer = new IntersectionObserver(handleIntersect, options);
-    observer.observe(target);
+    observer.observe(input);
   };
+
+targets.forEach(function(section){
+  createObserver(section)
+});
 
   
