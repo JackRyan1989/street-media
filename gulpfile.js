215ceddd842a4a895d6bdac4217624e0a9b2407a
JackRyan1989 <jackryan@navapbc.com>
2021-12-28 23:31:30 -0500

    Gulp file created, js minified and transpiled

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,31 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const sourcemaps = require("gulp-sourcemaps");
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+const inject = require('gulp-inject');
+
+function jsTask() {
+  return gulp
+    .src("./js/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(
+      babel({
+        presets: ["@babel/env"],
+      })
+    )
+    .pipe(concat("main.js"))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./js'))
   }
-  
-  exports.default = defaultTask
+
+  function injectStuff() {
+    let target = gulp.src('./index.html');
+    let sources = gulp.src(['./js/main.js'], {read: false});
+
+    return target.pipe(inject(sources))
+    .pipe(gulp.dest('./'))
+  }
+
+exports.build = gulp.series(jsTask, injectStuff);
