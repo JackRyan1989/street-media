321b14563ba39ad1d7abc154d7aa02f0c3e28a35
JackRyan1989 <jackryan@navapbc.com>
2022-01-01 15:06:54 -0500

    Minified CSS and concatenated

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 const gulp = require("gulp");
 const concat = require("gulp-concat");
+const cleanCss = require('gulp-clean-css');
 const sourcemaps = require("gulp-sourcemaps");
 const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
 const inject = require('gulp-inject');
+
+function cssTask() {
+  return gulp.src(["./stylesheets/reset.css", "./stylesheets/mobile.css", "./stylesheets/tablet.css", "./stylesheets/tablet_larger.css", "./stylesheets/desktop.css", "./stylesheets/parvus.css"])
+  .pipe(concat('main.css'))
+  .pipe(cleanCss({compatibility: 'ie11'}))
+  .pipe(gulp.dest("./stylesheets/"))
+}
 
 function jsTask() {
   return gulp
@@ -22,10 +30,10 @@ function jsTask() {
 
   function injectStuff() {
     let target = gulp.src('./index.html');
-    let sources = gulp.src(['./js/main.js'], {read: false});
+    let sources = gulp.src(['./stylesheets/main.css','./js/main.js'], {read: false});
 
     return target.pipe(inject(sources))
     .pipe(gulp.dest('./'))
   }
 
-exports.build = gulp.series(jsTask, injectStuff);
+exports.build = gulp.series(gulp.parallel(cssTask, jsTask), injectStuff);
